c840c81b92b91dccd8a22a6e7fce9e9ba82257d4
Howard He <nanfang2000@gmail.com>
2017-07-02 23:05:30 +0800

    Initial project that can run (without softdevice)

---
+++ Src/Device/nRF52/SoftDevice/hex/main.d
@@ -0,0 +1,3 @@
+..\..\..\..\..\..\components\softdevice\s132\hex\main.o: ..\..\..\main.c
+..\..\..\..\..\..\components\softdevice\s132\hex\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdbool.h
+..\..\..\..\..\..\components\softdevice\s132\hex\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
